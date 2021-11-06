89a6288ef72e4fa049dc23e2b4e87435c5466485
bahman <bahman026@gmail.com>
2021-11-06 22:51:49 +0330

    install laravel sanctum

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,14 @@ class LoginController extends Controller
     {
         return view('login.login');
     }
-    public function login(){
+    public function login(Request $request){
+        $request->validate([
+           'email'=>'required',
+           'password'=>'required|string|min:8',
+        ]);
+        $email = $request['email'];
+        $password = $request['password'];
+
         dd("tset");
     }
 }
